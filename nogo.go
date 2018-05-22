d0786983244491d35207ef955a1b19de5befa1f0
Angad Nadkarni <angadn@Angads-Hullo-MacBook-Pro.local>
2018-05-22 13:07:02 +0530

    First draft

---
+++ nogo.go
@@ -0,0 +1,51 @@
+package nogo
+
+import (
+	"sync"
+
+	"github.com/google/uuid"
+)
+
+type Block struct {
+	name    string
+	blocked []chan bool
+
+	l *sync.Mutex
+}
+
+func (b Block) Equals(o Block) bool {
+	return b.name == o.name
+}
+
+func (b Block) WithName(name string) Block {
+	b.name = name
+	return b
+}
+
+func (b Block) String() string {
+	return b.name
+}
+
+func NewBlock() Block {
+	ret := Block{}
+	ret.name = uuid.New().String()
+	return ret
+}
+
+func (b *Block) Wait() <-chan bool {
+	b.l.Lock()
+	defer b.l.Unlock()
+	ret := make(chan bool, 1)
+	b.blocked = append(b.blocked, ret)
+	return ret
+}
+
+func (b *Block) Done() {
+	b.l.Lock()
+	defer b.l.Unlock()
+	for _, ch := range b.blocked {
+		ch <- true
+	}
+
+	b.blocked = []chan bool{} // Let those channels get garbage collected
+}
