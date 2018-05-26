8ff903715cf76de3e9423493ace35fd86f6328a2
Angad Nadkarni <angadn@Angads-Hullo-MacBook-Pro.local>
2018-05-26 18:20:52 +0530

    Added a testing suite

---
+++ nogo_test.go
@@ -0,0 +1,34 @@
+package nogo_test
+
+import (
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/angadn/nogo"
+)
+
+func TestBlockAlreadyDone(t *testing.T) {
+	block := nogo.NewBlock()
+	block.Done()
+	block.Wait()
+	block.Wait()
+	block.Wait()
+}
+
+func TestBlockMultipleWait(t *testing.T) {
+	block := nogo.NewBlock()
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < 3; i++ {
+		wg.Add(1)
+		go func() {
+			<-block.Wait()
+			wg.Done()
+		}()
+	}
+
+	time.Sleep(1 * time.Second)
+	block.Done()
+	wg.Wait()
+}
